5387c25121c0a39f0aacf3e90a73cbfe3fd2b44e
planeset94 <planeset94@gmail.com>
2021-04-29 17:09:22 +0200

    EX. 2: asked even/odd & nuimber to the user

---
+++ JS/pari-dispari.js
@@ -0,0 +1,13 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+var even_Odd = prompt('Choose: even or odd. Write it here please');
+var user_Number = prompt('Enter a number between 1 and 5');
+
+console.log(even_Odd);
+console.log(user_Number);
+
+
+
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
