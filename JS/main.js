33065b10b522c7672ee33875b7c2284a8c5845c4
planeset94 <planeset94@gmail.com>
2021-04-29 16:42:49 +0200

    Added isPalindromo Function

--- JS/main.js
+++ JS/main.js
@@ -4,26 +4,41 @@
 // 1) Chiedo all'utende una parola 
 var user_word = prompt('Tell me a word');
 
+
 // 2) definisco la lunghezza della parola 
 var leng = user_word.length;
-console.log(leng);
+
 // 3) trovo metà lunghezza (arrotondando per difetto)
 var half_leng = Math.floor(leng / 2);
 console.log(half_leng);
 
+/**
+ * 
+ * @param {*} parola_utente : è la parola da analizzare. 
+ * @returns La funzione restituisce un valore vero o falso, a seconda che parola_utente sia palidroma o meno. 
+ */
 
 
-var palindromo = false;
-for (var i = 0; i < half_leng; i++) {
+function isPalindromo(parola_utente) {
 
-    var lett_left = user_word.charAt(i);
-    var lett_right = user_word.charAt(leng - (i + 1));
-    if (lett_left == lett_right) {
-        palindromo = true;
+    var palindromo = false;
+    for (var i = 0; i < half_leng; i++) {
+
+        var lett_left = user_word.charAt(i);
+        var lett_right = user_word.charAt(leng - (i + 1));
+        if (lett_left == lett_right) {
+            palindromo = true;
+        }
     }
 
-
-
+    return palindromo;
 }
 
-console.log(palindromo);
+var risultato = isPalindromo(user_word);
+if (risultato) {
+    document.getElementById('stampa_palindromi').innerHTML = "<strong>Congratulations!!</strong> The word you entered is palindrome";
+} else {
+    document.getElementById('stampa_palindromi').innerHTML = "The word you entered <strong> IS NOT </strong> palindrome";
+}
+
+
